09f55910d09abed988b9ed70ed83eddad010aaa5
Benedikt Fein <fein@fim.uni-passau.de>
2022-06-28 17:31:00 +0200

    handle nested test suites

--- src/main/java/de/tum/in/www1/jenkins/notifications/model/Testsuite.java
+++ src/main/java/de/tum/in/www1/jenkins/notifications/model/Testsuite.java
@@ -1,6 +1,8 @@
 package de.tum.in.www1.jenkins.notifications.model;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
@@ -29,6 +31,8 @@ public class Testsuite {
 
     @XmlAttribute
     private int tests;
+
+    private List<Testsuite> testSuites;
 
     private List<TestCase> testCases;
 
@@ -74,5 +78,60 @@ public class Testsuite {
     @XmlElement(name = "testcase")
     public void setTestCases(List<TestCase> testCases) {
         this.testCases = testCases;
+    }
+
+    @Exported
+    public List<Testsuite> getTestSuites() {
+        return testSuites;
+    }
+
+    @XmlElement(name = "testsuite")
+    public void setTestSuites(List<Testsuite> testSuites) {
+        this.testSuites = testSuites;
+    }
+
+    /**
+     * Combines the test suite and all child test suites recursively into a single test suite.
+     *
+     * @return This test suite with all children suites merged into it.
+     */
+    public Testsuite flatten() {
+        if (testSuites != null) {
+            testSuites.stream().map(Testsuite::flatten).forEach(this::addOther);
+
+            // make sure the testSuites are null, as they should not be exported in the JSON back to Artemis
+            testSuites = null;
+        }
+
+        return this;
+    }
+
+    /**
+     * Merges the other test suite into this one.
+     *
+     * @param other Some other test suite.
+     */
+    private void addOther(final Testsuite other) {
+        // the basic attributes of nested test suites are already aggregated here
+
+        if (this.testCases == null) {
+            this.testCases = new ArrayList<>();
+        }
+        if (other.testCases != null) {
+            List<TestCase> otherTestCases = other.testCases.stream().map(testCase -> {
+                testCase.setName(buildTestCaseName(other, testCase));
+                return testCase;
+            }).collect(Collectors.toList());
+            this.testCases.addAll(otherTestCases);
+        }
+    }
+
+    private static String buildTestCaseName(final Testsuite suite, final TestCase testCase) {
+        if (suite.name == null) {
+            return testCase.getName();
+        }
+        else {
+            return suite.name + '.' + testCase.getName();
+        }
     }
 }
