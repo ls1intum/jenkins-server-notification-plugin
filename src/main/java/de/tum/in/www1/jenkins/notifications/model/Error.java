e214ac0d09daee8460829c02156be25c6c302cd3
Benedikt Fein <fein@fim.uni-passau.de>
2024-11-07 17:44:46 +0100

    style: apply spotless to all files
    Apply to all files now, then the diffs will be easier to see in the
    future. Before, the ratcheting mechanism only formatted changed files as
    we went along.

--- src/main/java/de/tum/in/www1/jenkins/notifications/model/Error.java
+++ src/main/java/de/tum/in/www1/jenkins/notifications/model/Error.java
@@ -1,19 +1,21 @@
 package de.tum.in.www1.jenkins.notifications.model;
 
-import org.kohsuke.stapler.export.Exported;
-import org.kohsuke.stapler.export.ExportedBean;
-
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlValue;
+import org.kohsuke.stapler.export.Exported;
+import org.kohsuke.stapler.export.ExportedBean;
 
 @ExportedBean(defaultVisibility = 4)
 @XmlRootElement(name = "error")
 public class Error {
+
     @XmlAttribute
     private String message;
+
     @XmlValue
     private String messageWithStackTrace;
+
     @XmlAttribute
     private String type;
 
